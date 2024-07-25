a253d29f79729196a0f3265e5c1a5a1c59ba2076
adonijah01 <adonijahsingoei@gmail.com>
2024-07-25 14:34:36 +0300

    guessing_game

---
+++ guessing_game/target/debug/deps/guessing_game-c4201cfd4c20dbb6.d
@@ -0,0 +1,5 @@
+/home/arrow/Rust/guessing_game/target/debug/deps/guessing_game-c4201cfd4c20dbb6: src/main.rs
+
+/home/arrow/Rust/guessing_game/target/debug/deps/guessing_game-c4201cfd4c20dbb6.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/arrow/Rust/guessing_game/target/debug/guessing_game: /home/arrow/Rust/guessing_game/src/main.rs
